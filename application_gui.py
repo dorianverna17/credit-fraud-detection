3fdc4549071722b2894d3e54a879e84a13d5dc1a
Dorian <dorianverna7@gmail.com>
2021-09-26 22:07:49 +0300

    finished transaction add action and most of the predict command

--- application_gui.py
+++ application_gui.py
@@ -1,6 +1,14 @@
 import tkinter as tk
-from tkinter import BOTH, TOP
+from tkinter import BOTH, TOP, END
+import tkinter.messagebox
+
+import numpy as np
+import pandas as pd
 from PIL import ImageTk, Image
+
+import fraud_prediction as fp
+
+types = {'PAYMENT', 'DEBIT', 'CASH_OUT', 'TRANSFER', 'CASH_IN'}
 
 
 def resize_image(e):
@@ -12,6 +20,76 @@ def resize_image(e):
     image2 = ImageTk.PhotoImage(resized)
     canvas.create_image(0, 0, image=image2, anchor='nw')
     canvas.pack(fill=BOTH, expand=True)
+
+
+def add_transaction():
+    flag = 0
+    time_str = time_text.get('0.0', 'end')
+    time_str = time_str[0:len(time_str) - 1]
+    type_str = type_text.get('0.0', 'end')
+    type_str = type_str[0:len(type_str) - 1]
+    amount_str = amount_text.get('0.0', 'end')
+    amount_str = amount_str[0:len(amount_str) - 1]
+    name_orig_str = nameOrig_text.get('0.0', 'end')
+    name_orig_str = name_orig_str[0:len(name_orig_str) - 1]
+    old_balance_org_str = oldbalanceOrg_text.get('0.0', 'end')
+    old_balance_org_str = old_balance_org_str[0:len(old_balance_org_str) - 1]
+    new_balance_orig_str = newbalanceOrig_text.get('0.0', 'end')
+    new_balance_orig_str = new_balance_orig_str[0:len(new_balance_orig_str) - 1]
+    name_dest_str = nameDest_text.get('0.0', 'end')
+    name_dest_str = name_dest_str[0:len(name_dest_str) - 1]
+    old_balance_dest_str = oldbalanceDest_text.get('0.0', 'end')
+    old_balance_dest_str = old_balance_dest_str[0:len(old_balance_dest_str) - 1]
+    new_balance_dest_str = newbalanceDest_text.get('0.0', 'end')
+    new_balance_dest_str = new_balance_dest_str[0:len(new_balance_dest_str) - 1]
+    if time_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if type_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if amount_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if nameOrig_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if oldbalanceOrg_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if newbalanceOrig_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if nameDest_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if oldbalanceDest_text.get('0.0', 'end') == "\n":
+        flag = 1
+    if newbalanceDest_text.get('0.0', 'end') == "\n":
+        flag = 1
+    s = ""
+    try:
+        t_time = int(time_str)
+        t_amount = float(amount_str)
+        t_old_balance_orig = float(old_balance_org_str)
+        t_new_balance_orig = float(new_balance_orig_str)
+        t_old_balance_dest = float(old_balance_dest_str)
+        t_new_balance_dest = float(new_balance_dest_str)
+        s = s + str(t_time) + "  -  " + str(t_amount) + "  -  " + \
+            name_orig_str + "  -  " + str(t_old_balance_orig) + "  -  " + \
+            str(t_new_balance_orig) + "  -  " + name_dest_str + "  -  " + \
+            str(t_old_balance_dest) + "  -  " + str(t_new_balance_dest)
+    except ValueError as e:
+        flag = 1
+    if type_str not in types:
+        flag = 1
+    if flag == 1:
+        tk.messagebox.showinfo('Error', 'The transaction data have not been added')
+    else:
+        s = type_str + "  -  " + s
+        box.insert("end", s)
+
+
+def predict_transactions():
+    df_lst = []
+    for data in enumerate(box.get(0, END)):
+        lst = str(data).split("  -  ")
+        df_line = np.array(lst)
+        df_lst.append(df_line)
+    y = fp.final_model.predict(df_lst)
 
 
 window = tk.Tk()
@@ -69,10 +147,13 @@ canvas.create_window(500, 270, window=newbalanceDest_text)
 box = tk.Listbox(canvas, fg='white', bg='grey', height=10, width=120)
 canvas.create_window(400, 380, window=box)
 
-add_transaction_button = tk.Button(canvas, text="Add Transaction", height=1, width=20)
-predict_button = tk.Button(canvas, text="Predict", height=1, width=20)
+add_transaction_button = tk.Button(canvas, text="Add Transaction",
+                                   height=1, width=20, command=add_transaction)
+predict_button = tk.Button(canvas, text="Predict", height=1, width=20,
+                           command=predict_transactions)
 canvas.create_window(250, 490, window=add_transaction_button)
 canvas.create_window(550, 490, window=predict_button)
 
 window.resizable(0, 0)
+print('ok')
 window.mainloop()
